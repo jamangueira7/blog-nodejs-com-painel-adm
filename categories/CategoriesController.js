ac41c2d45777a120c539c1d9cb9c91755af91954
João Mangueira <jamjs7@gmail.com>
2021-03-18 20:37:50 -0300

    gravando categoria gravando

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
+const Category = require('./Category');
 
 router.get('/admin/categories/new', (req, res) => {
    res.render('admin/categories/new');
+});
+
+router.post('/categories/save', (req, res) => {
+   var title = req.body.title;
+
+   if(title !== undefined) {
+      Category.create({
+         title,
+         slug: slugify(title),
+      }).then(() => {
+         res.redirect('/');
+      });
+   }else {
+      res.redirect('/admin/categories/new');
+   }
 });
 
 module.exports = router;
