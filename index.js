8478b8fc1094d8cb7e6c88d90c531a17c7e95408
João Mangueira <jamjs7@gmail.com>
2021-03-18 19:06:25 -0300

    conexão feita

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const connection = require('./database/database');
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extends: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+connection
+    .authenticate().then(() => {
+        console.log('Conexão feita com sucesso');
+    }).catch((err) => {
+        console.log(err);
+    });
 
 app.get('/', (req, res) => {
     res.render('index');
