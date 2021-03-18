6aff0d0c8b866d0101965a819c52040ff7a2e5dd
João Mangueira <jamjs7@gmail.com>
2021-03-18 17:50:08 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extends: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+   console.log('O servidor está rodando!');
+});
