c5840c0249bc0328e043ec956b21916c482cfa12
João Mangueira <jamjs7@gmail.com>
2021-03-19 19:22:11 -0300

    add uma navbar para a home, add visualização a home e buscar e enviar os artigos para a home

--- index.js
+++ index.js
@@ -25,8 +25,13 @@ connection
 
 app.use('/', categoriesController);
 app.use('/', articlesController);
+
 app.get('/', (req, res) => {
-    res.render('index');
+    Article.findAll({
+        include: [{ model: Category }]
+    }).then((articles) => {
+        res.render('index', { articles });
+    });
 });
 
 app.listen(3000, () => {
