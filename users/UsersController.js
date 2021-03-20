32b8412ca33e33ad425093d17baac9df7121070b
João Mangueira <jamjs7@gmail.com>
2021-03-20 20:59:34 -0300

    add form para cadastro de usuario

--- users/UsersController.js
+++ users/UsersController.js
@@ -10,4 +10,12 @@ router.get('/admin/users/new', (req, res) => {
    res.render('admin/users/new');
 });
 
+router.post('/users/save', (req, res) => {
+   var email = req.body.email;
+   var password = req.body.password;
+
+   res.json({ email, password });
+   //res.render('admin/users/new');
+});
+
 module.exports = router;
