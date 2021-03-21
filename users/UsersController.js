2e0d93f07ab4b5198ecbad6e518b41c021cc35b1
João Mangueira <jamjs7@gmail.com>
2021-03-20 21:33:53 -0300

    usuario sendo criado

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./User');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
    res.send('Listagem de users')
@@ -14,8 +15,17 @@ router.post('/users/save', (req, res) => {
    var email = req.body.email;
    var password = req.body.password;
 
-   res.json({ email, password });
-   //res.render('admin/users/new');
+   var salt = bcrypt.genSaltSync(10);
+   var hash = bcrypt.hashSync(password, salt);
+
+   User.create({
+      email,
+      password: hash
+   }).then(() => {
+      res.redirect('/');
+   }).catch((err) => {
+      res.redirect('/');
+   });
 });
 
 module.exports = router;
